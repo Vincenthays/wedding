6963df620fe2d262e758946a343cc8596eba3a23
Vincent <vincent.hays@nielseniq.com>
2025-12-16 17:18:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const params = Object.fromEntries(new URLSearchParams(window.location.search));
+
+document.addEventListener('alpine:init', () => {
+    Alpine.store('params', params);
+    Alpine.store('wedding', {
+        date: new Date('jul 11, 2026 15:00:00').getTime(),
+        dateText: '11 juin 2026 • 15h00'
+    });
+});
+
