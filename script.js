c6526ff55bb6881a4c1734b5380f278419ada9ec
Vincent <vincent.hays@nielseniq.com>
2025-12-16 19:23:14 +0100

    clean up

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const params = Object.fromEntries(new URLSearchParams(window.location.search));
 document.addEventListener('alpine:init', () => {
     Alpine.store('params', params);
     Alpine.store('wedding', {
-        date: new Date('jul 11, 2026 15:00:00').getTime(),
+        date: new Date('june 11, 2026 15:00:00').getTime(),
         dateText: '11 juin 2026 • 15h00'
     });
 });
