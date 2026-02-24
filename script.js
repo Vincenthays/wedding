0a54288c85971498b863095ed417dfd156294279
Vincent <vincent.hays@nielseniq.com>
2026-02-24 22:44:58 +0100

    clean up

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Decode data from URL parameter
 const params = JSON.parse(atob(window.location.search.substring(1)));
 
+// Initialize Alpine.js store with wedding data
 document.addEventListener('alpine:init', () => {
     Alpine.store('params', { ...params, date: new Date(params.t) });
 });
